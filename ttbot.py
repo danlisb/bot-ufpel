ce739ca9fecc45e45737e6748cf7de8aead84d91
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-12-08 15:32:20 -0300

    Update ttbot.py

--- ttbot.py
+++ ttbot.py
@@ -19,9 +19,11 @@ agora = datetime.date.today() #Computa o dia de hoje
 fdias_horas = ferias - agora #Diferença entre os dias colocados anteriormente (Tempo para as férias)
 fdias = fdias_horas.days  #Agora pegamos somente os dias 
 
-if fdias != 1 and fdias != 0:
+if fdias > 1:
     api.update_status("Faltam " + str(fdias) + " dias para as férias da UFPEL")
 if fdias == 1:
     api.update_status("Falta " + str(fdias) + " dia para as férias da UFPEL!!!")
 if fdias == 0:
     api.update_status("Hoje começam as férias na UFPEL!!!")
+if fdias < 0:
+    pass
