f136c2fb2209e050efc5a33d22871eb4bacf312d
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-12-13 16:19:09 -0300

    Update ttbot.py

--- ttbot.py
+++ ttbot.py
@@ -1,23 +1,20 @@
 import tweepy
-import datetime 
+import datetime
 
-key =  ''  #Acess
+key =  '' #Acess
 secret = '' #Chave secreta
-
 consumer_key = '' #API
 consumer_secret = '' #Chave secreta
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
-
 api = tweepy.API(auth)
 
-
-ferias = datetime.date() #Colocar o dia das férias (ANO,MES,DIA)
+ferias = datetime.date(2023,5,12) #Colocar o dia das férias (ANO,MES,DIA)
 agora = datetime.date.today() #Computa o dia de hoje
 
 fdias_horas = ferias - agora #Diferença entre os dias colocados anteriormente (Tempo para as férias)
-fdias = fdias_horas.days  #Agora pegamos somente os dias 
+fdias = fdias_horas.days  #Agora pegamos somente os dias
 
 if fdias > 1:
     api.update_status("Faltam " + str(fdias) + " dias para as férias da UFPEL")
